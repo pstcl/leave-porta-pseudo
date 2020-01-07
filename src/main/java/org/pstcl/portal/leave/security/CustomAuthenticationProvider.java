8473e90eb384ee287b9c6a851a6b59233a0de754
pstcl <40257916+pstcl@users.noreply.github.com>
2020-01-07 18:44:01 +0530

    adsf

---
+++ src/main/java/org/pstcl/portal/leave/security/CustomAuthenticationProvider.java
@@ -0,0 +1,101 @@
+package org.pstcl.portal.leave.security;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+	
+	
+	private RestTemplate restTemplate;
+
+	public CustomAuthenticationProvider(	RestTemplateBuilder restTemplateBuilder)
+	{
+		this.restTemplate = restTemplateBuilder
+				.setConnectTimeout(Duration.ofSeconds(500))
+				.setReadTimeout(Duration.ofSeconds(500))
+				.build();
+	}
+ 
+    @Override
+    public Authentication authenticate(Authentication authentication) 
+      throws AuthenticationException {
+  
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+         
+        if (authorizeEmployee(username,password)) {
+  
+            // use the credentials
+            // and authenticate against the third-party system
+            return new UsernamePasswordAuthenticationToken(
+              username, password, new ArrayList<>());
+        } else {
+            return null;
+        }
+    }
+ 
+    
+
+	@Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+	
+	
+	public Boolean authorizeEmployee(String empid, String employeePassword)
+	{
+		Boolean authenticated=false;
+		
+		String username = "api_pstcl";
+		String password = "amritsarpatiala";
+		
+
+		String url = "https://hrapipstcl.pspcl.in/api/EmployeeAuthenticate";
+		String apiCredential = "Basic " + username + ":" + password + ":" + empid + ":" + employeePassword;
+		//    
+
+		// create headers
+		HttpHeaders headers = new HttpHeaders();
+		// set `accept` header
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		// set custom header
+		headers.set("Authorization", apiCredential);
+
+		// build the request
+		HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+
+		//		 ResponseEntity<Post> response = this.restTemplate.exchange(url, HttpMethod.GET, entity, Post.class, 1);
+		//	        if (response.getStatusCode() == HttpStatus.OK) {
+		//	            return response.getBody();
+		//	        } else {
+		//	            return null;
+		//	        }
+
+		ResponseEntity<String> response= restTemplate.exchange(url,HttpMethod.GET,entity,String.class,0);
+
+
+
+		if(response.getBody().toUpperCase().equalsIgnoreCase("TRUE"))
+		{
+			authenticated=true;
+		}
+		return authenticated;
+	}
+
+}
